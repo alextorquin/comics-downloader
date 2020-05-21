84e8e149b73c3cf8aa15e252ada0da8381d098a5
alextorquin <alextorquin@gmail.com>
2020-05-21 03:44:26 +0200

    Script de despliegue

---
+++ deploy_macos.sh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+
+DEST=/Users/alex/bin
+
+cp download_comics.sh $DEST
+cp comics_urls.txt $DEST
+cp comics-downloader $DEST
--- download_comics.sh
+++ download_comics.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 
-
 URL=($(cat comics_urls.txt | tr -s "\n" ","))
-./comics-downloader -format=cbr -url=$URL -output=Prueba
+
+comics-downloader -format=cbr -url=$URL -output=/Volumes/Almacen/Prueba
