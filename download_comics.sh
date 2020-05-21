23bd61b5bcb238b3355c9dd832e72cb58c3b5c77
alextorquin <alextorquin@gmail.com>
2020-05-21 13:36:51 +0200

    .

--- download_comics.sh
+++ download_comics.sh
@@ -2,4 +2,4 @@
 
 URL=($(cat comics_urls.txt | tr -s "\n" ","))
 
-comics-downloader -format=cbr -url=$URL -output=/Volumes/Almacen/Prueba
+./comics-downloader -format=cbr -url=$URL -output=/Volumes/Almacen/Muelle1/Comics
