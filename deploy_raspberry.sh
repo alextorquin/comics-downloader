dc7be8f5d1b353c16f219aba18c181671818e5d5
alextorquin <alextorquin@gmail.com>
2020-06-27 02:21:36 +0200

    .

--- deploy_raspberry.sh
+++ deploy_raspberry.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 DEST=/home/pi/bin/comics-downloader
+SRC=/home/pi/Trabajo/comics-downloader
+
+go build -o comics-downloader $SRC/cmd/downloader
 
 cp download_comics.sh $DEST
 cp comics_urls.txt $DEST
