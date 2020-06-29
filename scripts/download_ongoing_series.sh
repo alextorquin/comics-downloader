9bbdb9cb55c9eb4532118a2ecab8b5dae4542038
alextorquin <alextorquin@gmail.com>
2020-06-30 00:37:31 +0200

    .

--- scripts/download_ongoing_series.sh
+++ scripts/download_ongoing_series.sh
@@ -6,7 +6,7 @@ DEST="/home/pi/TorPort/Filmoteca/Comictor"
 # obtener directorio de este script
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-URL=($(cat "$URL_PATH/complete_series.txt" | tr -s "\n" ","))
+URL=($(cat "$URL_PATH/ongoing_series.txt" | tr -s "\n" ","))
 
 $DIR/comics-downloader -format=cbr -url=$URL -output=$DEST > $DIR/comics-downloader.out
 
