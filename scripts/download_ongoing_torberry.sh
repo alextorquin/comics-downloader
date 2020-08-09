3337ac4a0cf9d116af4645f1118958661b9954bb
alextorquin <alextorquin@gmail.com>
2020-08-09 12:04:52 +0200

    .

--- scripts/download_ongoing_torberry.sh
+++ scripts/download_ongoing_torberry.sh
@@ -3,6 +3,8 @@
 # obtener directorio de este script
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+$DIR/../utils/pushbullet.sh "Descarga de comics iniciada."
+
 $DIR/download_comics.sh ongoing.txt
 
 $DIR/../utils/pushbullet.sh "Descarga de comics finalizada."
